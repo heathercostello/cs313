2d9399e6fa2f8c6ca750962e9aaa69b2aca7b99b
costelloheather <heathercostello6@gmail.com>
2020-01-23 22:26:45 -0700

    change3434384

--- web/assignments/week3/shopping-cart/view/browse.php
+++ web/assignments/week3/shopping-cart/view/browse.php
@@ -36,7 +36,7 @@
 			?>
 				<div class="product-item">
 					<form method="post" action="index.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
-					<div class="product-image"><a href="index.php?action=details&code=<?php echo $product_array[$key]["code"]; ?>"><img src="<?php echo $product_array[$key]["image"]; ?>"></a></div>
+					<div class="product-image"><a href="index.php?action=details&code=<?php echo $product_array[$key]["code"]; ?>"><img class="image_size" src="<?php echo $product_array[$key]["image"]; ?>"></a></div>
 					<div><strong><?php echo $product_array[$key]["name"]; ?></strong></div>
 					<div class="product-price"><?php echo "$".$product_array[$key]["price"]; ?></div>
 					<div><input type="text" name="quantity" value="1" size="2" /><input type="submit" value="Add to cart" class="btnAddAction" /></div>
