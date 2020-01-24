f9627462ef1c66c40fcf3ee93525a1bf1f8cb91b
costelloheather <heathercostello6@gmail.com>
2020-01-23 23:41:27 -0700

    change34343843344

--- web/assignments/week3/shopping_cart/tblproduct.sql
+++ web/assignments/week3/shopping_cart/tblproduct.sql
@@ -1,13 +1,13 @@
-CREATE TABLE IF NOT EXISTS `tblproduct` (
-  `id` int(8) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `image` text NOT NULL,
-  `price` double(10,2) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `product_code` (`code`)
-)
+-- CREATE TABLE IF NOT EXISTS `tblproduct` (
+--   `id` int(8) NOT NULL AUTO_INCREMENT,
+--   `name` varchar(255) NOT NULL,
+--   `image` text NOT NULL,
+--   `price` double(10,2) NOT NULL,
+--   PRIMARY KEY (`id`),
+--   UNIQUE KEY `product_code` (`code`)
+-- )
 
-INSERT INTO `tblproduct` (`id`, `name`, `image`, `price`) VALUES
-(1, 'Instant Pot', 'assignment-img/instantpot.png', 100.00),
-(2, 'Kitchen Aid', 'assignment-img/kitchenaid.png', 600.00),
-(3, 'Blend Tec', 'assignment-img/blendtec.png', 200.00);
+-- INSERT INTO `tblproduct` (`id`, `name`, `image`, `price`) VALUES
+-- (1, 'Instant Pot', 'assignment-img/instantpot.png', 100.00),
+-- (2, 'Kitchen Aid', 'assignment-img/kitchenaid.png', 600.00),
+-- (3, 'Blend Tec', 'assignment-img/blendtec.png', 200.00);
