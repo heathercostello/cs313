ad4c54e19b14e2de18b36573e705951d297edfd6
costelloheather <heathercostello6@gmail.com>
2020-02-14 01:13:52 -0700

    changing database

--- web/assignments/groceryListProject/newGroceryList.php
+++ web/assignments/groceryListProject/newGroceryList.php
@@ -18,8 +18,19 @@ $db = get_db();
 $user_id = htmlspecialchars($_POST['user_id']);
 $content = htmlspecialchars($_POST['grocery_list_content']);
 
-echo "$user_id\n";
-echo $content;
+require('dbConnect.php');
+$db = get_db();
+
+$stmt = $db->prepare('INSERT INTO grocery_list(user_table_id, grocery_list_name, content) VALUES
+(:user_id, :content);');
+$stmt->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+$stmt->bindValue(':content', $content, PDO::PARAM_STR);
+$stmt->execute();
+
+$new_page = "userGroceryLists.php?id=$user_id";
+header("Location: $new_page");
+die();
+
 ?>
 
 <!DOCTYPE html>
