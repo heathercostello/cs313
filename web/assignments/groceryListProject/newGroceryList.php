b7536075dd143c96628d26557351da5a0ea5977f
costelloheather <heathercostello6@gmail.com>
2020-02-15 21:32:53 -0700

    taking out grocery name

--- web/assignments/groceryListProject/newGroceryList.php
+++ web/assignments/groceryListProject/newGroceryList.php
@@ -2,20 +2,20 @@
 $user_id = htmlspecialchars($_POST['user_id']);
 $list_content = htmlspecialchars($_POST['list_content']);
 
-// echo "$user_id\n";
-// echo $list_content;
+echo "$user_id\n";
+echo $list_content;
 
-require('dbConnect.php');
-$db = get_db();
+// require('dbConnect.php');
+// $db = get_db();
 
-$stmt = $db->prepare('INSERT INTO grocery_list(user_table_id, list_content) VALUES
-(:user_table_id, :list_content);');
-$stmt->bindValue(':user_table_id', $user_table_id, PDO::PARAM_INT);
-$stmt->bindValue(':list_content', $list_content, PDO::PARAM_STR);
-$stmt->execute();
+// $stmt = $db->prepare('INSERT INTO grocery_list(user_table_id, list_content) VALUES
+// (:user_table_id, :list_content);');
+// $stmt->bindValue(':user_table_id', $user_table_id, PDO::PARAM_INT);
+// $stmt->bindValue(':list_content', $list_content, PDO::PARAM_STR);
+// $stmt->execute();
 
-$new_page = "userGroceryLists.php?id=$user_id";
-header("Location: $new_page");
-die();
+// $new_page = "userGroceryLists.php?id=$user_id";
+// header("Location: $new_page");
+// die();
 
 ?>
