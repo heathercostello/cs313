d56a3232f3cb45948e584cbb2b1583f3b99da7df
costelloheather <heathercostello6@gmail.com>
2020-02-15 21:56:49 -0700

    taking out grocery name

--- web/assignments/groceryListProject/newGroceryList.php
+++ web/assignments/groceryListProject/newGroceryList.php
@@ -1,21 +1,33 @@
 <?php
+require('dbConnect.php');
+$db = get_db();
+
 $user_id = htmlspecialchars($_POST['user_id']);
 $list_content = htmlspecialchars($_POST['list_content']);
 
 // echo "$user_id\n";
-// echo $list_content;
-
-require('dbConnect.php');
-$db = get_db();
+ echo $list_content;
 
 $stmt = $db->prepare('INSERT INTO grocery_list(user_table_id, list_content) VALUES
 (:user_table_id, :list_content);');
+
+echo "ONE";
+
 $stmt->bindValue(':user_table_id', $user_table_id, PDO::PARAM_INT);
+
+echo "TWO";
+
 $stmt->bindValue(':list_content', $list_content, PDO::PARAM_STR);
+
+echo "THREE";
+
 $stmt->execute();
+
+echo "FOUR";
+
 
 $new_page = "userGroceryLists.php?user_id=$id";
 header("Location: $new_page");
-die();
+// die();
 
 ?>
