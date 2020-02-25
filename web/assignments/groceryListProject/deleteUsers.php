44fc8c9349f7e06a4c7b90af2f4a2468c4092ea1
costelloheather <heathercostello6@gmail.com>
2020-02-24 23:48:55 -0700

    changing database

---
+++ web/assignments/groceryListProject/deleteUsers.php
@@ -0,0 +1,42 @@
+<?php
+// Start the session
+session_start();
+?>
+<?php
+require('dbConnect.php');
+$db = get_db();
+
+$query = 'SELECT id, username, first_name, last_name FROM user_table';
+$stmt = $db->prepare($query);
+$stmt->execute();
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+//SELECT id, username, first_name, last_name FROM user_table;
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Grocery List Users</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Delete User</h1>
+    
+    <form method="post" action="delete_user.php">
+        <input type="hidden" name="username" value="<?php echo $_GET["username"]; ?>">
+        <div class="input-group">
+        User Name: <input type="text" name="username" value="<?php echo $username;?>"><br>
+        </div>
+        <div class="input-group">
+        First Name: <input type="text" name="firstName" value="<?php echo $firstName;?>"><br>
+        </div>
+        <div class="input-group">
+        Last Name: <input type="text" name="lastName" value="<?php echo $lastName;?>"><br>
+        </div>
+        <input class="btn" type="submit" value="Delete User">
+        </form>
+
+        <p class="btnTwo"><a href="users.php">Go back to list of users</a></p>
+
+</body>
+</html>
